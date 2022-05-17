e3cc44baffc9fe4e9206bcb478ee06c7f49fddf6
Bruno Bengtson Neves <brunobn13@hotmail.com>
2022-05-17 09:36:50 -0300

    feat: Criação theme global configurado na raiz (root)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@material-ui/core/styles'
+
+import { theme } from 'styles/theme';
+
+import 'index.css';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,22 @@
+import { ptBR } from '@material-ui/core/locale'
+import { createTheme } from '@material-ui/core/styles'
+
+import { sg } from './styleGuide'
+
+export const theme = createTheme(
+  {
+    //TODO: adicionar paleta de cores, espaçamentos, ...
+    typography: {
+      allVariants: {
+        fontFamily: sg.fontFamily.primary
+      },
+      h1: { fontSize: sg.heading[1] },
+      h2: { fontSize: sg.heading[2] },
+      h3: { fontSize: sg.heading[3] },
+      h4: { fontSize: sg.heading[4] },
+      h5: { fontSize: sg.heading[5] },
+      h6: { fontSize: sg.heading[6] }
+    },
+  },
+  ptBR
+)
